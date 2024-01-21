a087d1686d72d9f085d1a16757cc07645986b6b7
C-r0 <146779565+C-r0@users.noreply.github.com>
2024-01-21 10:31:09 -0300

    Add files via upload

---
+++ Game.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    // Variaveis
+    public GameObject X;
+    public GameObject Bola;
+    public GameObject Q1;
+    public GameObject Q2;
+    public GameObject Q3;
+    public GameObject Q4;
+    public GameObject Q5;
+    public GameObject Q6;
+    public GameObject Q7;
+    public GameObject Q8;
+    public GameObject Q9;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Jogo();
+    }
+
+    void Jogo()
+    {
+        
+    }
+}
